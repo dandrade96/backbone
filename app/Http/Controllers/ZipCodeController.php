03818c3dacb507039939f9ef24267ffd62c2cd03
Diêgo C. Andrade <diego.andrade@dortech-group.com>
2022-08-30 16:16:51 -0300

    RTT

--- app/Http/Controllers/ZipCodeController.php
+++ app/Http/Controllers/ZipCodeController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\ConnectionException;
 
 class ZipCodeController extends Controller
 {
     public function index(Request $request){
-        return $response = Http::get('https://jobs.backbonesystems.io/api/zip-codes/'.$request->zip_code)->json();
+        $promise = Http::get('https://jobs.backbonesystems.io/api/zip-codes/'.$request->zip_code);
+
+        return $promise->json();
     }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,8 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::pattern('zip_code', '[0-9]+');
         });
     }
 
