0ab3992b3b7ae8eb178a4ace32b90d0e1cbe34bc
Diêgo C. Andrade <diego.andrade@dortech-group.com>
2022-09-01 11:04:11 -0300

    Add call database

--- app/Http/Controllers/ZipCodeController.php
+++ app/Http/Controllers/ZipCodeController.php
@@ -6,9 +6,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\ConnectionException;
 
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+use App\Models\ZipCode;
+
 class ZipCodeController extends Controller
 {
     public function index(Request $request){
-        return $promise = Http::get('https://jobs.backbonesystems.io/api/zip-codes/'.$request->zip_code);
+        $response = ZipCode::where('code',$request->zip_code)->first();
+        if($response){
+            return json_decode($response->data);
+        }
+
+        $promise = Http::get('https://jobs.backbonesystems.io/api/zip-codes/'.$request->zip_code)->body();
+
+        $rawdata = [
+            'code' => $request->zip_code,
+            'data' => $promise,
+        ];
+        // return $rawdata;
+        ZipCode::create($rawdata);
+        return json_decode($promise);
     }
-}
+}   
